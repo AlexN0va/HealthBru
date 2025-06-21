3bb69bdfd38d06c2270baad891859e27d6caa125
Davyn Paringkoan <davyn.par@gmail.com>
2025-06-21 12:51:41 -0700

    added basic groq

---
+++ backend/groqtest.py
@@ -0,0 +1,30 @@
+import os
+
+from groq import Groq
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        # Set an optional system message. This sets the behavior of the
+        # assistant and can be used to provide specific instructions for
+        # how it should behave throughout the conversation.
+        {
+            "role": "system",
+            "content": "You are a helpful assistant."
+        },
+        # Set a user message for the assistant to respond to.
+        {
+            "role": "user",
+            "content": "What's a nutritional meal that includes bread and eggs",
+        }
+    ],
+
+    # The language model which will generate the completion.
+    model="llama-3.3-70b-versatile"
+)
+
+# Print the completion returned by the LLM.
+print(chat_completion.choices[0].message.content)
